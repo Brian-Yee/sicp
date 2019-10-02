ce97e5d431300e059b7d9e04f14079d4503904e1
Brian-Yee <brian.ph.yee@gmail.com>
2019-10-02 17:30:52 -0400

    Refactor Simpsons Rule code.

--- 04-simpsons/main.scm
+++ 04-simpsons/main.scm
@@ -1,9 +1,37 @@
+; Simple Riemann Sum numerical integration of f(x)
 (define (integral f a b dx)
-  (define (add-dx x)
+  (define (next x)
     (+ x dx))
-  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+  (define start (+ a (/ dx 2.0)))
+  (* (sum f start next b)
      dx))
 
+; Simpsons Method to numerically integrate f(x)
+(define (simpsons f a b n)
+  ; Infinitesimal spacing approximation
+  (define h (/ (- b a) n))
+
+  ; Special cases
+  (define first (f a))
+  (define penultimate (* 4 (f (- b h))))
+  (define last (f b))
+
+  ; Repeating case
+  (define (inner a b)
+    (define start (+ a h))
+    (define end (- b h h))
+
+    (define (next x) (+ x h h))
+    (define (pattern x)
+      (+
+       (* 4 (f (+ x 0)))
+       (* 2 (f (+ x h)))))
+    (sum pattern start next end))
+
+  (* (/ h 3)
+     (+ first (inner a b) penultimate last)))
+
+; Shared functions between numerical integration methods
 (define (cube x)
   (* x x x))
 
@@ -12,26 +40,3 @@
       0
       (+ (term a)
          (sum term (next a) next b))))
-
-(define (inc n) (+ n 1))
-
-(define (simpsons f a b n)
-  (define h (/ (- b a) n))
-  (define (add-2h x) (+ x (* 2 h)))
-
-  (define (outer a b) (+ (f a) (f b)))
-
-  (define (pattern x)
-    (+ (* 4 (f (+ x 0)))
-       (* 2 (f (+ x h)))))
-
-  (define (inner a b)
-    (sum pattern a add-2h b))
-
-  (define (edge b)
-    (* 4 (f (- b h))))
-
-  (* (/ h 3)
-     (+ (outer a b) (edge b) (inner (+ a h) (- b h h)))))
-
-(define (wtf n) (+ n 1))
