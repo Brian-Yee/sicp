6873911ccc0f0353e1dd7649bd13967a509e4008
Brian-Yee <brian.ph.yee@gmail.com>
2019-10-03 08:27:30 -0400

    Refactor using new let command.

--- 04-simpsons/main.scm
+++ 04-simpsons/main.scm
@@ -1,35 +1,34 @@
 ; Simple Riemann Sum numerical integration of f(x)
 (define (integral f a b dx)
-  (define (next x)
-    (+ x dx))
-  (define start (+ a (/ dx 2.0)))
-  (* (sum f start next b)
-     dx))
+  (let ((start (+ a (/ dx 2.0)))
+        (end b))
+    (* (sum f start (lambda (x) (+ x dx)) end)
+       dx))
+  ))
 
 ; Simpsons Method to numerically integrate f(x)
 (define (simpsons f a b n)
   ; Infinitesimal spacing approximation
   (define h (/ (- b a) n))
 
-  ; Special cases
-  (define first (f a))
-  (define penultimate (* 4 (f (- b h))))
-  (define last (f b))
-
   ; Repeating case
   (define (inner a b)
     (define start (+ a h))
     (define end (- b h h))
-
-    (define (next x) (+ x h h))
-    (define (pattern x)
+    (define (function x)
       (+
        (* 4 (f (+ x 0)))
        (* 2 (f (+ x h)))))
-    (sum pattern start next end))
 
-  (* (/ h 3)
-     (+ first (inner a b) penultimate last)))
+    (sum function start (lambda (x) (+ x h h)) end))
+
+  ; Special cases
+  (let ((first (f a))
+        (penultimate (* 4 (f (- b h))))
+        (last (f b)))
+    (* (/ h 3)
+       (+ first (inner a b) penultimate last))
+    ))
 
 ; Shared functions between numerical integration methods
 (define (cube x)
